352770f84c80bc3a940f1736ed6b8e2b1053af97
Gary Holler <gholler76@gmail.com>
2021-04-18 17:12:28 -0400

    first commit - assignment complete

---
+++ src/main/java/com/holler/authentication/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.holler.authentication.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.holler.authentication.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+	User findByEmail(String email);
+
+
+
+
+}
